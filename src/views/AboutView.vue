ba3ec7e5b2df65cf3f21dba8f1551e575a60384a
gymcoding <bruce.lean17@gmail.com>
2023-11-15 13:59:31 +0900

    3일 차 실전편 - Vue Router 설치완료

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>About View</h2>
+    <p>{{ $route.path }}</p>
+    <button class="btn btn-primary" @click="moveHomeView">
+      Home 으로 이동
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter, useRoute } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+console.log('route.path: ', route.path);
+const moveHomeView = () => router.push('/');
+</script>
+
+<style lang="scss" scoped></style>
